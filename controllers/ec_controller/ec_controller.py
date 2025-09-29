5b04ed6fb1a38f40f43301706e075d9a3846c9b5
Felipe Gomes de Melo <felipegmelo@usp.br>
2025-09-29 10:35:29 -0300

    Add claw controller

--- controllers/ec_controller/ec_controller.py
+++ controllers/ec_controller/ec_controller.py
@@ -8,13 +8,16 @@ from typing import cast
 
 import zmq
 from controller import Motor, Robot
-from pmr_elirobots_msgs.cmd import JointCommandMsg
+from pmr_elirobots_msgs.cmd import CommandMsg
+from pmr_elirobots_msgs.types import ClawState
 
 # create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
+
+MAX_LINEAR_POSITION = 0.01
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -24,15 +27,26 @@ timestep = int(robot.getBasicTimeStep())
 
 joint_list = [cast(Motor, robot.getDevice(f"joint{i}")) for i in range(1, 7)]
 
+claw_actuators = [
+    cast(Motor, robot.getDevice(f"linear_motor_{i}")) for i in range(1, 3)
+]
+
 
 def receive_callback(raw_msg: list[bytes]):
     payload = raw_msg[1].decode("utf-8")
 
-    msg = JointCommandMsg.from_json(payload)
+    msg = CommandMsg.from_json(payload)
 
     for n, angle in enumerate(msg.cmd.as_list):
         if angle is not None:
             joint_list[n].setPosition(angle / 180 * math.pi)
+
+    if msg.cmd.claw == ClawState.OPEN:
+        claw_actuators[0].setPosition(0)
+        claw_actuators[1].setPosition(0)
+    elif msg.cmd.claw == ClawState.CLOSE:
+        claw_actuators[0].setPosition(MAX_LINEAR_POSITION)
+        claw_actuators[1].setPosition(MAX_LINEAR_POSITION)
 
 
 context = zmq.Context()
